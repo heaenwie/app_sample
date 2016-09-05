4474384b2c271d4e7997ba6d02d622b0f1f45069
heaenwie <marta.eleonora.ciesielska@gmail.com>
2016-09-05 22:06:29 +0100

    Adding flash and tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                  email: "user@invalid",
+                                  password:              "foo",
+                                  password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div.field_with_errors'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+    assert_select 'div#error_explanation'
+  end
+
+  test "valid signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                  email: "user@valid.com",
+                                  password:              "foobar",
+                                  password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+    assert_not 'flash.alert-danger'
+    #assert_not 'div.field_with_errors'
+  end
+end
+
+# test "the truth" do
+  #   assert true
+  # end
