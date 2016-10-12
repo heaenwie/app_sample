2aea309390987cf1947ed5c8b394bb1f9299edfb
heaenwie <marta.eleonora.ciesielska@gmail.com>
2016-10-12 18:54:34 +0200

    Small fix

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -11,7 +11,7 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     get new_password_reset_path
     assert_template 'password_resets/new'
     #invalid email test
-    #post password_reset_path, params: { password_reset: { email: "" } }
+    #post password_reset_path, params: { password_reset: { email: "" } } <-- nie działa 
     #assert_not flash.empty?
     #assert_template 'password_resets/new'
     # Valid email
